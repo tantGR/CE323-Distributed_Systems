b3e45e9f681ac1c0f0ad912884dcd517d7f22982
Christos Axelos <caxelos@inf.uth.gr>
2019-02-26 03:46:08 +0200

    copy Thodori's code and adding saveInRequestFile() function

--- HW1/client.py
+++ HW1/client.py
@@ -1,7 +1,32 @@
-import clientMW as mw
+import clientMW as mw 
+import struct
+svcid = 5
 
 
-mw.sendRequest(10,2,1)
+def main():
+    print("Client APP started.\n")
+    while True:
+        ans = int(input("What to do next?\n(1) to give a new number/(2) to get an answer/(3) to exit: "))
+        if ans==1:
+            num = int(input("Give an number: "))
+            buf = struct.pack('i',num)
+            reqid = mw.sendRequest(svcid,buf,len(buf))
+        elif ans==2:
+            num = int(input("For which nunber? "))
+            blk = input("Do you want to wait until I have the answer?(y/n) ")
+            if blk=='y':
+                block = True
+            else:
+                block = False
+                res = mw.getReply(reqid,num,len,block)	
+        elif ans==3:
+            break
+        
 
+if __name__ == "__main__":
+    main()
 
-
+'''to do: dict for ids-nums
+          unpack reply
+		  print reply : Is prime or not 
+'''
--- HW1/clientMW.py
+++ HW1/clientMW.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import threading
-
-ids = 0
-Req = 0
-Repl = 0
-
+import struct
+nlife=0;
+reqid=0;
+svcid=50;
 reqs_dict = {}
 
+
 def Requests():
-	print "requests" , ids
+	print("requests\n") 
 
 
 def Replies():
-	print "replies"
+	print("replies\n")
 
 
 class MyThread(threading.Thread):
@@ -20,22 +23,55 @@ class MyThread(threading.Thread):
 		self.threadID = threadID
 		self.name = name
 		self._funcToRun = funcToRun
-		self._args = args
+		self._args = args#empty
 	def run(self):
 		self._funcToRun(*self._args)
 
 
-def sendRequest(svcid, buf, len):
-	global ids, Repl, Req, reqs_dict
-	if not(Req != 0 and Repl!=0 and Req.isAlive() and Repl.isAlive()):
-		Req = MyThread(Requests, 1, "Requests")
-		Repl = MyThread(Replies, 2, "Replies")
-		Req.start()
-		Repl.start()
+def saveInRequestFile(reqid,svcid,buf,len,nlife):
+    try:
+        # log file to write to
+        reqsFile = r'reqsFile.txt'
+        data = (reqid,svcid,buf,len,nlife)
+        format_string = "%s,%s,%s,%s,%s\n"
+        print(format_string % data)
 
-	ids += 1
-	reqs_dict[ids] = (svcid,buf,len) 
-	return ids
+        #'a' means append at endOfFile 
+        with open(reqsFile, 'a') as f:
+            # write the new request and it's meta-data
+            f.write(format_string % data)
+    except IOError:
+        print("Error writing at Request File.Exiting\n")
+        return -1
+     
+    f.close()
+    return 0
+
+def sendRequest(svcid, buf, len):
+	#global ids, Repl, Req, reqs_dict
+    
+    #Apothikefsi tou Request sto arxeio. Eggrafes tis morfis(reqid,svcid,buf,len,nlife)
+    if (saveInRequestFile(reqid,svcid,buf,len,nlife)==-1):
+        return -1
+    
+    #Prosthiki sto dictionary
+    reqs_dict[reqid]=(svcid,buf,len,nlife)     
+        
+    #Discover UDP Multicast()
+    
+    #Discovery apo ton Client
+    
+    
+    print("reqid is: "+str(reqid)+"\n")
+    
+#    if not(Req != 0 and Repl!=0 and Req.isAlive() and Repl.isAlive()):
+#        Req = MyThread(Requests, 1, "Requests")
+#        Repl = MyThread(Replies, 2, "Replies")
+#        Req.start()
+#        Repl.start()
+#    ids += 1
+#    reqs_dict[ids] = (svcid,buf,len) 
+#    return ids
 
 def getReply(reqid, buf, len, block):
-	print 
+	print("popa") 
